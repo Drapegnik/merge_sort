66d1b6bc86fb2163c284ff80cd28509e5ca4b3a3
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2016-09-25 14:46:40 +0300

    add setting range of values for gen script

--- gen.py
+++ gen.py
@@ -6,6 +6,6 @@ import random
 f = open('input.txt', 'w')
 f.write(sys.argv[1] + '\n')
 for i in range(int(sys.argv[1])):
-    f.write(str(random.randint(0, 100)) + ' ')
+    f.write(str(random.randint(0, int(sys.argv[2]))) + ' ')
 
 f.close()
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python gen.py "$1"
+python gen.py "$1" "$2"
 mpicxx -o merge_sort main.cpp
-mpirun -n "$2" merge_sort
+mpirun -n "$3" merge_sort
